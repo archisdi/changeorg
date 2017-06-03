3855ffaa646e7d253f3dda72e71202ce122217ed
archisdiningrat <archie.isdiningrat@gmail.com>
2017-06-03 22:42:27 +0700

    pertemuan 6

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/',function (){
+   return response('welcome to change.org api',200);
+});
+
+
+Route::resource('news','NewsController');
